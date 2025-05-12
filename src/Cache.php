a69d39200305f6a0415be8c6e0f62ced4796203c
wilkques <wilkques@gmail.com>
2025-05-12 15:33:22 +0800

    add connection

--- src/Cache.php
+++ src/Cache.php
@@ -3,27 +3,17 @@
 namespace Wilkques\Cache;
 
 use Wilkques\Container\Container;
-use Wilkques\Helpers\Arrays;
 
 class Cache
 {
-    /**
-     * @var string
-     */
-    protected $driver = 'file';
-
-    /**
-     * driver resolver
-     * 
-     * @var array
-     */
-    protected $store = array();
-
     /**
      * @var Container
      */
     protected $container;
 
+    /**
+     * @param Container $container
+     */
     public function __construct(Container $container)
     {
         $this->container = $container;
@@ -37,27 +27,17 @@ class Cache
         return (new \Wilkques\Container\Container)->make('\Wilkques\Cache\Cache');
     }
 
-    /**
-     * @param string $driver
-     * 
-     * @return FileStore
-     */
-    public function driver($driver = 'file')
-    {
-        switch ($driver) {
-            case 'file':
-            default:
-                Arrays::set($this->store, $driver, $this->container->make('\Wilkques\Cache\FileStore'));
-                break;
-        }
-
-        return Arrays::get($this->store, $driver);
-    }
-
     public function __call($method, $arguments)
     {
-        $store = $this->driver($this->driver);
-        
+        /** @var \Wilkques\Cache\Connections\Connection */
+        $connection = (new \Wilkques\Container\Container)->make('\Wilkques\Cache\Connections\Connection');
+
+        if ($method == 'driver') {
+            return call_user_func_array(array($connection, 'driver'), $arguments);
+        }
+
+        $store = $connection->driver();
+
         return call_user_func_array(array($store, $method), $arguments);
     }
 
---
+++ src/Connections/Connection.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Wilkques\Cache\Connections;
+
+use Wilkques\Container\Container;
+use Wilkques\Helpers\Arrays;
+
+class Connection
+{
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @var string
+     */
+    protected $driver = 'file';
+
+    /**
+     * driver resolver
+     * 
+     * @var array
+     */
+    protected $store = array();
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param string $driver
+     * 
+     * @return FileStore
+     */
+    public function driver($driver = 'file')
+    {
+        $this->driver = $driver;
+
+        switch ($driver) {
+            case 'file':
+            default:
+                if ($this->hasnotStore()) {
+                    Arrays::set($this->store, $driver, $this->container->make('\Wilkques\Cache\FileStore'));
+                }
+                break;
+        }
+
+        return $this->store();
+    }
+
+    /**
+     * @param string|null $driver
+     * 
+     * @return FileStore
+     */
+    public function store($driver = null)
+    {
+        $driver = $driver ?: $this->driver;
+
+        return Arrays::get($this->store, $driver);
+    }
+
+    /**
+     * @param string|null $driver
+     * 
+     * @return bool
+     */
+    public function hasStore($driver = null)
+    {
+        return (bool) $this->store($driver);
+    }
+
+    /**
+     * @param string|null $driver
+     * 
+     * @return bool
+     */
+    public function hasnotStore($driver = null)
+    {
+        return ! $this->hasStore($driver);
+    }
+}
